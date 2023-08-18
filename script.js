06c7a7c0cfbb0ca85ad478025aadbfcfd5db67a8
Tar556q <134805275+Tar556q@users.noreply.github.com>
2023-08-18 15:45:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 document.getElementById('loadMore').addEventListener('click', function() {
-    
-    const skillsSection = document.querySelector('section:nth-child(3) ul');
-
-   
-    const newSkills = ['backend develober'];
-
-    
-    newSkills.forEach(skill => {
-        const listItem = document.createElement('li');
-        listItem.textContent = skill;
-        skillsSection.appendChild(listItem);
-    });
-
-    
-    this.style.display = 'none';
+    alert('Tareq Asarwi CV');
 });
